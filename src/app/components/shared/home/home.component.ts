a76f16422e4ba886d572fed99d746392ac8d3a2a
Paul Landaeta <paulwilkerlf@gmail.com>
2020-06-07 13:19:18 -0400

    set default image

--- src/app/components/shared/home/home.component.ts
+++ src/app/components/shared/home/home.component.ts
@@ -26,4 +26,10 @@ export class HomeComponent implements OnInit {
   createHospital() {
     this.router.navigate(['hospital-new']);
   }
+
+  getImage(hospital: Hospital) {
+    return hospital.photoURL
+      ? hospital.photoURL
+      : 'https://previews.123rf.com/images/neyro2008/neyro20081512/neyro2008151200317/49781838-ilustraci%C3%B3n-de-la-ciudad-del-edificio-del-hospital-en-el-estilo-de-dise%C3%B1o-plano-arquitectura-cl%C3%ADnica-hospita.jpg';
+  }
 }
